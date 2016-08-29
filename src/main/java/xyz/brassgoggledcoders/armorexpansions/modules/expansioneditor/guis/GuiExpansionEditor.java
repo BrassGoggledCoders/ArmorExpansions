277691d3e57f2346eeff194e406d40e7764e4eaf
warlordjones <warlordjones@gmail.com>
2016-08-29 12:34:58 +0100

    Use GuiInventory.drawEntityOnScreen for player rendering, pending component fixes

--- src/main/java/xyz/brassgoggledcoders/armorexpansions/modules/expansioneditor/guis/GuiExpansionEditor.java
+++ src/main/java/xyz/brassgoggledcoders/armorexpansions/modules/expansioneditor/guis/GuiExpansionEditor.java
@@ -1,6 +1,7 @@
 package xyz.brassgoggledcoders.armorexpansions.modules.expansioneditor.guis;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
@@ -9,18 +10,19 @@ import xyz.brassgoggledcoders.armorexpansions.ArmorExpansions;
 import xyz.brassgoggledcoders.armorexpansions.modules.expansioneditor.containers.ContainerExpansionEditor;
 import xyz.brassgoggledcoders.armorexpansions.modules.expansioneditor.tileentities.TileEntityExpansionEditor;
 import xyz.brassgoggledcoders.boilerplate.client.guis.GuiScreenBase;
-import xyz.brassgoggledcoders.boilerplate.client.guis.components.RenderEntityLivingBaseComponent;
 
 @SideOnly(Side.CLIENT)
 public class GuiExpansionEditor extends GuiScreenBase {
-	private static ResourceLocation guiTexture = new ResourceLocation(ArmorExpansions.NAME, "textures/gui/extension_editor.png");
+	private static ResourceLocation guiTexture =
+			new ResourceLocation(ArmorExpansions.NAME, "textures/gui/extension_editor.png");
 
 	public GuiExpansionEditor(EntityPlayer player, TileEntityExpansionEditor extensionEditor) {
 		super(new ContainerExpansionEditor(player, extensionEditor));
 		int x = (this.width - this.xSize) / 2;
 		int y = (this.height - this.ySize) / 2;
-		this.addComponent(new RenderEntityLivingBaseComponent(x + 33, y + 75, 30, (float) (x + 33) - x,
-				(float) (y + 75 - 50) - y, Minecraft.getMinecraft().thePlayer));
+		// TODO
+		// this.addComponent(new RenderEntityLivingBaseComponent(x + 33, y + 75, 30, (float) (x + 33) - x,
+		// (float) (y + 75 - 50) - y, Minecraft.getMinecraft().thePlayer));
 	}
 
 	@Override
@@ -30,5 +32,7 @@ public class GuiExpansionEditor extends GuiScreenBase {
 		int y = (this.height - this.ySize) / 2;
 		this.drawTexturedModalRect(x, y, 0, 0, this.xSize, this.ySize);
 		super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+		GuiInventory.drawEntityOnScreen(x + 33, y + 75, 30, (float) (x + 33) - mouseX, (float) (y + 75) - mouseY,
+				Minecraft.getMinecraft().thePlayer);
 	}
 }
